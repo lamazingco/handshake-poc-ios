2992940de28ba9e086a1c8a699bebe770c4dba25
Marin <marintutucv@gmail.com>
2019-05-24 17:42:50 +0300

    Initial Commit

---
+++ handshake-poc-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handshake-poc-ios
+//
+//  Created by Marin Tutuc on 5/24/19.
+//  Copyright © 2019 Marin Tutuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
